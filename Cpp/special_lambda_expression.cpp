7c5a3d8420bee48cdee2d96dc558a09d7c79887a
Elvis Zhai <elvis.zhai@zoom.us>
2023-12-13 15:25:08 +0800

    lambda insights

--- Cpp/special_lambda_expression.cpp
+++ Cpp/special_lambda_expression.cpp
@@ -43,3 +43,83 @@ int main()
 
     return 0;
 }
+
+/*
+    Produced by complier:
+    #include <iostream>
+#include <functional>
+
+class ConfigMgr
+{
+  
+  private: 
+  bool setting_mute;
+  bool setting_rename;
+  int * data;
+  
+  public: 
+  inline ConfigMgr(bool _mute, bool _rename)
+  : setting_mute{_mute}
+  , setting_rename{_rename}
+  {
+    this->data = new int{722};
+  }
+  
+  inline ~ConfigMgr() noexcept
+  {
+    if(nullptr != this->data) {
+      delete this->data;
+      this->data = nullptr;
+    } 
+    
+  }
+  
+  inline void print_current_setting()
+  {
+    std::operator<<(std::operator<<(std::cout.operator<<(std::boolalpha), " setting - mute: ").operator<<(this->setting_mute), '\n');
+    std::operator<<(std::operator<<(std::cout.operator<<(std::boolalpha), " setting - rename: ").operator<<(this->setting_rename), '\n');
+    std::operator<<(std::operator<<(std::cout, "data is: ").operator<<(*this->data), '\n');
+  }
+  
+};
+
+int main()
+{
+  ConfigMgr * config_mgr = new ConfigMgr{true, false};
+  std::operator<<(std::operator<<(std::cout, "The address of config_mgr is: ").operator<<(reinterpret_cast<const void *>(config_mgr)), '\n');
+    
+  class __lambda_31_33
+  {
+    public: 
+    inline constexpr  void operator()() const
+    {
+      std::operator<<(std::cout, "[mgr()] begin\n");
+      std::operator<<(std::operator<<(std::cout, "The address of config_mgr is: ").operator<<(reinterpret_cast<const void *>(config_mgr)), '\n');
+      if(nullptr != config_mgr) {
+        config_mgr->print_current_setting();
+      } 
+      
+      std::operator<<(std::cout, "[mgr()] end\n");
+    }
+    
+    private: 
+    ConfigMgr * config_mgr;
+    public: 
+    // inline constexpr  __lambda_31_33(const __lambda_31_33 &) noexcept = default;
+    // inline constexpr  __lambda_31_33(__lambda_31_33 &&) noexcept = default;
+    __lambda_31_33(ConfigMgr * _config_mgr)
+    : config_mgr{_config_mgr}
+    {}
+    
+  };
+  
+  std::function<void ()> mgr = std::function<void ()>(__lambda_31_33{config_mgr});
+  mgr.operator()();
+  delete config_mgr;
+  config_mgr = nullptr;
+  std::operator<<(std::operator<<(std::cout, "The address of config_mgr is: ").operator<<(reinterpret_cast<const void *>(config_mgr)), '\n');
+  mgr.operator()();
+  return 0;
+}
+
+*/
