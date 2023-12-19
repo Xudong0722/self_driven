2f1db8137e8f939e68a9987596f06338df7c7747
Elvis Zhai <elvis.zhai@zoom.us>
2023-12-19 17:14:12 +0800

    rvo&nrvo

--- Cpp/rvo_nrvo_learning.cpp
+++ Cpp/rvo_nrvo_learning.cpp
@@ -5,7 +5,7 @@ private:
     int data;
 public:
     Test(int _data = 0) : data(_data){
-        std::cout << "[Test]\n";
+        std::cout << "[Test]" << _data << "\n";
     }
 
     Test(const Test& test){
@@ -24,9 +24,13 @@ public:
 };
 
 //NRVO: Named return value optimization
-Test CreateTestDataWithNRVO(){
+Test CreateTestDataWithNRVO(bool condition){
     Test test{100};
-    return test;
+    if(!condition){
+        std::cout << "condition is " << condition << '\n';
+        return Test{};
+    }
+    return std::move(test);   // https://stackoverflow.com/questions/14856344/when-should-stdmove-be-used-on-a-function-return-value
 }
 
 //RVO:return 
@@ -36,7 +40,7 @@ Test CreateTestDataWithRVO(){
 
 int main()
 {
-    Test test_nrvo = CreateTestDataWithNRVO();
+    Test test_nrvo = CreateTestDataWithNRVO(true);
 
     Test test_rvo = CreateTestDataWithRVO();
 
