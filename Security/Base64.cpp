28676c221a4086cdecfdffc24d9515a793421fcb
Xudong0722 <xudong722@126.com>
2024-03-18 21:48:23 +0800

    Base64

--- Security/Base64.cpp
+++ Security/Base64.cpp
@@ -9,8 +9,8 @@ namespace utility{
         auto raw_length = raw_string.size();
         if(0 == raw_length) return false;
         out = "";
-        int equal_count{0};
-        if((8 * raw_length) % 6 != 0) equal_count = (6 - (8 * raw_length) % 6) / 2;
+        int padding_count{0};
+        if((8 * raw_length) % 6 != 0) padding_count = (6 - (8 * raw_length) % 6) / 2;
         std::string binary_string;
         for(int i = 0; i < raw_length; ++i){
             std::string single_char{"00000000"};
@@ -22,22 +22,36 @@ namespace utility{
             binary_string += single_char;
         }
         
-        for(int i = 0; i < raw_length * 8; i += 6){
-            int start = i, end = std::min(i+6, static_cast<int>(raw_length * 8));
+        binary_string += std::string(padding_count * 2, '0');
+        if(binary_string.size() < 24){
+            binary_string += std::string(24 - binary_string.size(), '0');
+        }
+        auto binary_string_length = binary_string.length();
+        //std::cout << binary_string << " " << binary_string.size() << '\n';
+
+        for(int i = 0; i < binary_string_length; i += 6){
+            int start = i, end = std::min(i+6, static_cast<int>(binary_string_length));
             int result{0};
             while(start < end){
                 result |= (binary_string[start] - '0');
                 if(++start < end) result <<= 1;
             }
+            std::cout << i << " " << end << " " << result << " " << encode_table[result] << '\n';
             out += encode_table[result];
         }
 
-        out += std::string(equal_count, '=');
+        out += std::string(padding_count, BASE64_PADDING);
         return true;
     }
 
     bool Base64Decode(const std::string& raw_string, std::string& out){
-
+        auto raw_string_length = raw_string.length();
+        if(0 == raw_string_length) return false;
+        
+        out = "";
+        for(int i = 0; i<raw_string_length; ++i){
+            
+        }
         return true;
     }
 };
