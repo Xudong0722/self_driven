246cb5ecb73012a5e3fe885e5c81b50fb1983da0
Elvis Zhai <elvis.zhai@zoom.us>
2024-03-18 18:26:36 +0800

    Base64 imple

--- Security/Base64.cpp
+++ Security/Base64.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <algorithm>
 
 namespace utility{
     static const char encode_table[] = {"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"};
@@ -9,11 +10,29 @@ namespace utility{
         if(0 == raw_length) return false;
         out = "";
         int equal_count{0};
-        if((8 * raw_length) % 6 != 0) equal_count = (8 - (8 * raw_length) % 6) / 2;
-        
+        if((8 * raw_length) % 6 != 0) equal_count = (6 - (8 * raw_length) % 6) / 2;
+        std::string binary_string;
         for(int i = 0; i < raw_length; ++i){
-            
+            std::string single_char{"00000000"};
+            int offset = static_cast<int>(raw_string[i]);
+            for(int j = 0; j < 8; ++j){
+                if((offset >> j) & 1)
+                    single_char[7-j] = '1';
+            }
+            binary_string += single_char;
         }
+        
+        for(int i = 0; i < raw_length * 8; i += 6){
+            int start = i, end = std::min(i+6, static_cast<int>(raw_length * 8));
+            int result{0};
+            while(start < end){
+                result |= (binary_string[start] - '0');
+                if(++start < end) result <<= 1;
+            }
+            out += encode_table[result];
+        }
+
+        out += std::string(equal_count, '=');
         return true;
     }
 
@@ -25,7 +44,7 @@ namespace utility{
 
 int main()
 {
-    std::string test_string{"HelloWorld"};
+    std::string test_string{"My name is elvis"};
     
     std::string base64_encode_result;
     if(utility::Base64Encode(test_string, base64_encode_result)){
