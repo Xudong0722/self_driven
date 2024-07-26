a2892ef0ed6518357e3e0f047b3900000450cb6e
elvis.zhai <elvis.zhai@zoom.us>
2024-07-26 16:50:03 +0800

    shared

--- Cpp/shared_ptr_ref_count.cpp
+++ Cpp/shared_ptr_ref_count.cpp
@@ -1,5 +1,6 @@
 #include <memory>
 #include <iostream>
+#include <vector>
 
 class A{
 public:
@@ -9,6 +10,11 @@ public:
     ~A() {
         std::cout << __FUNCTION__ << std::endl;
     }
+    int getaa() const{
+        return aa;
+    }
+private:
+    int aa{101};
 };
 
 void funcA(std::shared_ptr<A> a){
@@ -21,6 +27,24 @@ void funcB(const std::shared_ptr<A>& a){
     std::cout << a.use_count() << std::endl;
 }
 
+
+std::vector<std::shared_ptr<A>> vec;
+
+class C{
+public:
+    C(const std::shared_ptr<A>& a)
+     :pa(a){
+        std::cout << __FUNCTION__ << std::endl;
+    }
+    ~C(){
+        std::cout << __FUNCTION__ << " " << pa.use_count() << std::endl;
+    }
+    void print(){
+        std::cout << __FUNCTION__ << " " << pa.use_count() << " " << pa->getaa() << std::endl;
+    }
+private:
+    std::shared_ptr<A> pa;
+};
 int main()
 {
     std::cout << "Main start\n";
@@ -42,5 +66,16 @@ int main()
 
     std::cout << "Main end\n";
 
+
+    std::shared_ptr<A> b = std::make_shared<A>();
+
+    vec.push_back(b);
+    std::cout << b.use_count() << std::endl;
+    C c(b);
+    std::cout << b.use_count() << std::endl;
+    vec.clear();
+    std::cout << b.use_count() << std::endl;
+    b.reset();
+    c.print();
     return 0;
 }
